73e1022215052a94f66405fc1c01342b4dc3d932
Stephan Martin <martin.stephan9218@gmail.com>
2026-07-24 22:06:03 +0200

    create 3 animals npc with datas

---
+++ vite-project/src/modules/models/npc.js
@@ -0,0 +1,23 @@
+export const animals = {
+    "roar": {
+        "label":"Sanglier",
+        "pv": 80,
+        "hit": 2.5,
+        "force": 6,
+        "loot": [ "roar_skin", "roar_meat"]
+    },
+    "rabbit": {
+        "label":"Lapin",
+        "pv": 30,
+        "hit": 4.5,
+        "force": 2,
+        "loot": [ "rabbit_skin", "rabbit_meat"]
+    },
+    "deer": {
+        "label":"Cerf",
+        "pv": 50,
+        "hit": 3,
+        "force": 6,
+        "loot": [ "deer_skin", "deer_meat"]
+    }
+};
